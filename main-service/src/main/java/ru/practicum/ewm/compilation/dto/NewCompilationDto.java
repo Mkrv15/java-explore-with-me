ae8adc60340872ecbb58e566b47f08529d35451c
Mkrv15 <ilya15040@yandex.ru>
2026-08-06 00:54:38 +0400

    fix

--- main-service/src/main/java/ru/practicum/ewm/compilation/dto/NewCompilationDto.java
+++ main-service/src/main/java/ru/practicum/ewm/compilation/dto/NewCompilationDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.ewm.compilation.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -16,6 +17,7 @@ public class NewCompilationDto {
 
     private Boolean pinned;
     @NotNull
+    @NotBlank
     @Size(min = 1, max = 50)
     private String title;
     private List<Long> events = new ArrayList<>();
