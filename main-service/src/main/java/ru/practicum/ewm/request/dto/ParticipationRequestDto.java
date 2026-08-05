892672e090f10601318a48c6c7794a792f49d31c
Mkrv15 <ilya15040@yandex.ru>
2026-08-06 00:14:49 +0400

    fix

--- main-service/src/main/java/ru/practicum/ewm/request/dto/ParticipationRequestDto.java
+++ main-service/src/main/java/ru/practicum/ewm/request/dto/ParticipationRequestDto.java
@@ -1,8 +1,11 @@
 package ru.practicum.ewm.request.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -10,7 +13,8 @@ import lombok.NoArgsConstructor;
 public class ParticipationRequestDto {
 
     private Long id;
-    private String created;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime created;
     private Long event;
     private Long  requester;
     private String status;
