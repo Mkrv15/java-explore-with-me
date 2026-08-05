424a6f7c88e25b421b177efbabcfa3b1beb4d6b0
Mkrv15 <ilya15040@yandex.ru>
2026-08-06 01:10:03 +0400

    fix

--- stat-service/stat-server/src/main/java/ru/practicum/ewm/stats/service/StatServiceImpl.java
+++ stat-service/stat-server/src/main/java/ru/practicum/ewm/stats/service/StatServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.ewm.stats.dto.EndpointHitDto;
 import ru.practicum.ewm.stats.dto.ViewStatsDto;
+import ru.practicum.ewm.stats.error.ValidationException;
 import ru.practicum.ewm.stats.mapper.EndpointHitMapper;
 import ru.practicum.ewm.stats.model.EndpointHit;
 import ru.practicum.ewm.stats.repository.StatsRepository;
@@ -29,17 +30,17 @@ public class StatServiceImpl implements StatService {
     public List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
 
         if (start == null) {
-            throw new IllegalArgumentException("Start date cannot be null");
+            throw new ValidationException("Start date cannot be null");
         }
         if (end == null) {
-            throw new IllegalArgumentException("End date cannot be null");
+            throw new ValidationException("End date cannot be null");
         }
 
         if (end.isBefore(start)) {
-            throw new IllegalArgumentException("Дата старта должна быть раньше даты окончания");
+            throw new ValidationException("Дата старта должна быть раньше даты окончания");
         }
         if (end.equals(start)) {
-            throw new IllegalArgumentException("Дата старта должна быть раньше даты окончания");
+            throw new ValidationException("Дата старта должна быть раньше даты окончания");
         }
 
         return unique ? statsRepository.findUniqueStats(start, end, uris)
